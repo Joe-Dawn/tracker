4e98ff668e86185a85e1e7dd799a65b12e6cb459
Joe-Dawn <81428982+Joe-Dawn@users.noreply.github.com>
2024-04-09 22:29:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-import { keepAlive } from './keepalive.js';
-const newSessionId = '4073522d-ff9a-44b8-bef1-6447cbef5e08';
+import requests
+import time
 
-document.cookie = `session=${newSessionId}; domain=www.kogama.com; path=/;`;
+from keepalive import keepAlive
 
-fetch('https://www.kogama.com/', {
-  method: 'GET',
-  headers: {
-    'Cookie': `session=${newSessionId}`
-  }
-});
+new_session_id = '4073522d-ff9a-44b8-bef1-6447cbef5e08'
 
-keepAlive();
+response = requests.get('https://www.kogama.com/', cookies={'session': new_session_id})
 
-function sendPing(url) {
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Cookie': `session=${newSessionId}`
-    }
-  });
-}
+# Call the keepAlive function
+keepAlive()
 
-setInterval(() => {
-  sendPing('https://www.kogama.com/user/143157/pulse/');
-}, 1000);
+# Function to send a ping to the given URL
+def send_ping(url):
+    response = requests.get(url, cookies={'session': new_session_id})
+    print(f"Response from {url}: {response.status_code}")
+
+# Call the send_ping function with the desired URL every 1 second
+while True:
+    send_ping('https://www.kogama.com/user/143157/pulse/')
+    time.sleep(1)
